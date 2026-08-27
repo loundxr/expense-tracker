cdcb0fd4430e6422de1beb1ddddad9661037e4ec
Andrei Litvinovich <loounderr@gmail.com>
2026-08-27 14:30:11 +0300

    Fixed file logs, SlogTeeHandler

--- cmd/expense-tracker/main.go
+++ cmd/expense-tracker/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	core_logger "github.com/loundxr/expense-tracker/internal/core/logger"
+)
 
 func main() {
-	fmt.Println("expense-tracker app")
+	logCfg, err := core_logger.NewConfig()
+	if err != nil {
+		fmt.Printf("failed to load logger config: %v", err)
+		os.Exit(1)
+	}
+	logger := core_logger.Setup(logCfg)
+
+	logger.Info("expense-tracker app", slog.String("env", logCfg.Env))
 }
--- internal/core/logger/logger.go
+++ internal/core/logger/logger.go
@@ -6,9 +6,11 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/loundxr/expense-tracker/internal/core/logger/handlers/slogpretty"
+	"github.com/loundxr/expense-tracker/internal/core/logger/handlers/teehandler"
 )
 
 const (
@@ -34,42 +36,37 @@ type Logger struct {
 }
 
 func Setup(cfg Config) *slog.Logger {
-	var log *slog.Logger
+	var handlers []slog.Handler
 
-	localLogPath := os.Getenv("LOCAL_LOG_PATH")
+	if cfg.Env == envLocal {
+		handlers = append(handlers, setupPrettyHandler(os.Stdout))
+	} else {
+		handlers = append(handlers, slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
 
-	var output io.Writer = os.Stdout
+	logDir := os.Getenv("LOCAL_LOG_DIR")
+	if cfg.Env == envLocal && logDir != "" {
+		if err := os.MkdirAll(logDir, 0755); err == nil {
+			timestamp := time.Now().Format("2006-01-02T15-04-05.000")
+			fileName := filepath.Join(logDir, fmt.Sprintf("%s.log", timestamp))
 
-	if cfg.Env == envLocal && localLogPath != "" {
-		if err := os.MkdirAll(filepath.Dir(localLogPath), 0755); err == nil {
-			file, err := os.OpenFile(localLogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
 			if err == nil {
-				output = io.MultiWriter(os.Stdout, file)
+				fileHandler := slog.NewTextHandler(file, &slog.HandlerOptions{Level: slog.LevelDebug})
+				handlers = append(handlers, fileHandler)
 			}
 		}
 	}
 
-	switch cfg.Env {
-	case envLocal:
-		log = setupPrettySlog(output)
-	case envDev:
-		log = slog.New(
-			slog.NewJSONHandler(output, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
-	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(output, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	}
-	return log
+	combinedHandler := teehandler.New(handlers...)
+	return slog.New(combinedHandler)
 }
 
-func setupPrettySlog(out io.Writer) *slog.Logger {
+func setupPrettyHandler(out io.Writer) slog.Handler {
 	opts := slogpretty.PrettyHandlerOptions{
 		SlogOpts: &slog.HandlerOptions{
 			Level: slog.LevelDebug,
 		},
 	}
-	handler := opts.NewPrettyHandler(out)
-	return slog.New(handler)
+	return opts.NewPrettyHandler(out)
 }
